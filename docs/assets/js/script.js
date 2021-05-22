1d544310336ecf5cd840f992ae39b53acb41d674
Igor Ageev <igor@ageev.online>
2021-05-22 19:41:48 +0300

    Comments

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,7 +1,8 @@
 (function (window) {
     let classes = [];
     
-    // Изменение каждого списка
+    // Обновление классов блочного элемента #sentence
+    // при изенении любого списка на странице
     document.querySelectorAll('select').forEach(item => {
         let index = classes.push(item.value) - 1;
         item.addEventListener('change', event => {
